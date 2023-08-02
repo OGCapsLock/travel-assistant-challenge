c05227159de0d2183dbaabe450efcd202bcc8423
Osvaldo Cuambe <95348230+OGCapsLock@users.noreply.github.com>
2023-08-02 19:31:58 +0000

    Add search select combo box

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { Button } from 'reactstrap'
+import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <>
-    <Button color='danger'>
-      ola
-    </Button>
-   </>
-  )
+    <div className="h-auto">
+      <Row>
+        <Form>
+          <FormGroup className="d-flex flex-column align-items-center justify-content-center">
+            <Label for="cityName">City</Label>
+          </FormGroup>
+          <FormGroup className="d-flex justify-content-center gap-3">
+            <Input
+              id="cityName"
+              name="city"
+              placeholder="Ex:Maputo"
+              maxLength={60}
+              className="w-25"
+              type="text"
+            />
+            <Button>Search</Button>
+          </FormGroup>
+        </Form>
+      </Row>
+      <Row>
+        <Col className="bg-dark text-center text-white">
+          Designed By Osv aldo Cuambe
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default App
+export default App;
